4c9f739487cc201afe10b3b001d740a06fc595d0
JPL <52734496+jplemonias@users.noreply.github.com>
2020-02-06 02:10:32 +0100

    Update test2.js

--- js/test2.js
+++ js/test2.js
@@ -1,5 +1,4 @@
-
-      window.console = window.console || function(t) {};
-      if (document.location.search.match(/type=embed/gi)) {
-        window.parent.postMessage("resize", "*");
-      }
+window.console = window.console || function(t) {};
+if (document.location.search.match(/type=embed/gi)) {
+            window.parent.postMessage("resize", "*");
+}
