93f1c5fc13bb075480ed602329fec53bd9984604
JPL <52734496+jplemonias@users.noreply.github.com>
2020-02-06 11:11:57 +0100

    ajout paraallax.js

---
+++ js/parallax.js
@@ -0,0 +1,28 @@
+        // Modal Image Gallery
+function onClick(element) {
+    document.getElementById("img01").src = element.src;
+    document.getElementById("modal01").style.display = "block";
+    var captionText = document.getElementById("caption");
+    captionText.innerHTML = element.alt;
+}
+
+// Change style of navbar on scroll
+window.onscroll = function () { myFunction() };
+function myFunction() {
+    var navbar = document.getElementById("Navbar");
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+           navbar.className = "nav-bar" + " card" + " animate-top" + " blanc";
+    } else {
+        navbar.className = navbar.className.replace(" card animate-top blanc", "");
+    }
+}
+
+// Used to toggle the menu on small screens when clicking on the menu button
+function toggleFunction() {
+    var x = document.getElementById("Demo");
+    if (x.className.indexOf("voir") == -1) {
+        x.className += " voir";
+    } else {
+        x.className = x.className.replace(" voir", "");
+    }
+}
